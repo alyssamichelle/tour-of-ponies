399a024f93b7bb8304e25db5a0ddc4e642e81395
Alyssa Nicoll <alyssaamnell@gmail.com>
2021-09-21 14:07:32 -0500

    add more pony avatars

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -12,15 +12,15 @@ export class Hero {
   largeAvatar?: string;
 
   // augment hero data with avatar
-  public getAvatar(heroId): string {
-    let avatarLink = 'assets/mlp-avatars/pony-' + heroId + '.png';
+//   public getAvatar(heroId): string {
+//     let avatarLink = 'assets/mlp-avatars/pony-' + heroId + '.png';
 
-    // assign a random number between 21 - 53
-    if (heroId > 10) {
-      let randomNumber = Math.floor(Math.random() * 32) + 21;
-      avatarLink = 'assets/mlp-avatars/pony-' + randomNumber + '.png';
-    }
+//     // assign a random number between 21 - 53
+//     if (heroId > 10) {
+//       let randomNumber = Math.floor(Math.random() * 32) + 21;
+//       avatarLink = 'assets/mlp-avatars/pony-' + randomNumber + '.png';
+//     }
 
-    return avatarLink;
-  }
-}
+//     return avatarLink;
+//   }
+// }
