fb5e876a5add9dae21469075a43219a57dfb6d66
Alyssa Nicoll <alyssaamnell@gmail.com>
2021-09-21 14:24:24 -0500

    got turtles help to add this sucker to array start

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -31,7 +31,12 @@ export class HeroesComponent implements OnInit {
     if (!name) { return; }
     this.heroService.addHero({ name } as Hero)
       .subscribe(hero => {
-        this.heroes.push(hero);
+        // this.heroes.push(hero);
+        this.heroes.splice(0, 0, hero); // adds the name into the beginning of the array
+        this.heroes = this.heroes.slice(); // sets the array to a copy of itself
+        // Apparently, unless you're looping through it, Angular's change detection only notices changes to the array reference itself, not the items within it. So any place where you're simply passing the heroes variable along to something and not looping through it in that something, that copy of the array doesn't get updated. — Z
+        // Note: You might also want/need to do this in the ListViewComponent as well, not sure if that's supposed to be doing the same thing and/or has the same issue. — Z
+        // Another Note: Because I'm sure you'll ask, I googled "angular array binding not updating" and the very first result was a SO post where someone was having the same issue you were seeing. Left the tab open. The accepted answer also says something about it being fixed in a later version of Angular, but that was years ago and obviously it's still not working, sooooo /shrug.  — Z
       });
   }
 
