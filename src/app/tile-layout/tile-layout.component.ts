66c18393c518c711c23e17c913fd26f1c7d32847
Alyssa Nicoll <alyssaamnell@gmail.com>
2021-09-21 14:54:14 -0500

    pass in heroes binding to tilelayout

--- src/app/tile-layout/tile-layout.component.ts
+++ src/app/tile-layout/tile-layout.component.ts
@@ -1,26 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'tile-layout',
   templateUrl: './tile-layout.component.html',
   styleUrls: ['./tile-layout.component.scss'],
 })
-export class TileLayoutComponent implements OnInit {
-  public heroes: Hero[];
-
-  constructor(private heroService: HeroService) { }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => {
-        this.heroes = heroes.filter((hero) => !!hero.avatar)
-      });
-  }
-
+export class TileLayoutComponent {
+  @Input() public heroes: Hero[];
 }
