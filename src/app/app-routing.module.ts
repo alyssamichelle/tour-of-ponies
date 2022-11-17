1c639edabcf42775fbbe9d3513455712b6e39e2d
Alyssa Nicoll <anicoll@ffy2dqvy4l.lan>
2022-11-17 13:40:26 -0600

    update to Angular v14

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
-const routes = [
+const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: "dashboard", component: DashboardComponent, text: "Dashboard", icon: "home" },
